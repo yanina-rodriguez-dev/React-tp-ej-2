e7b2fd19d57667fdee76dbca1e7c1be3cb26632d
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-01 05:07:05 -0300

    agrego mawuetado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import './App.css';
+import Titulos from './components/Titulos';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   
 
   return (
     <>
-     
+     <Titulos></Titulos>
     </>
   )
 }
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,11 @@
+
+
+const Titulos = () => {
+    return (
+        <section className="container">
+            <h1 className="mt-5">¡Hello world</h1>
+        </section>
+    );
+};
+
+export default Titulos;
